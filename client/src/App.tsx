954f061c76588500e1086b3ca58125fcb82e40c6
David-mwas <dmwas704@gmaIl.com>
2025-01-11 23:32:57 +0300

    ADD(CORE): add TodoList,TodoItem,TodoForm components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
-import { Button, Container, Stack } from "@chakra-ui/react";
+import { Container, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
@@ -6,7 +9,7 @@ function App() {
       <Navbar />
       <Container>
         <TodoForm />
-        <TodoList/>
+        <TodoList />
       </Container>
     </Stack>
   );
