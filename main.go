2946c70bbc9b7872be6ebf2e08c6355dc67fa28c
David-mwas <dmwas704@gmaIl.com>
2025-01-11 23:58:28 +0300

    FIX(FEAT):fix cors errors -> allow origins in go server

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -52,6 +53,12 @@ func main() {
 	collection = client.Database(os.Getenv("MONGODB_DB")).Collection("todos")
 
 	app := fiber.New()
+
+	// cors middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173/",
+		AllowHeaders: "Origin,Content-Type,Accept",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
