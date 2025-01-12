ffa81567c58fe5d0995ba394917e54428e3b38c9
David-mwas <dmwas704@gmaIl.com>
2025-01-12 21:33:52 +0300

    FIX cors errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,10 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+// export const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+
+export const BASE_URL = "https://godoist.vercel.app/api";
 function App() {
   return (
     <Stack h="100vh">
