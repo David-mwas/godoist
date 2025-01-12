c7b80e2e5db837a8036e3bd3234b98350e3d2c97
David-mwas <dmwas704@gmaIl.com>
2025-01-12 16:23:33 +0300

    try vercel deploy gettodos

--- api/index.go
+++ api/index.go
@@ -1,10 +1,43 @@
 package handler
- 
 import (
-  "fmt"
-  "net/http"
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
- 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+type Todo struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body"`
+}
+
+var collection *mongo.Collection
+
+func GetTodos(c *fiber.Ctx) error {
+	todos := []Todo{}
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	defer cursor.Close(context.Background())
+	for cursor.Next(context.Background()) {
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		todos = append(todos, todo)
+
+	}
+	return c.JSON(todos)
+
 }
