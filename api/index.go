5951cf1927e07fbc8416b9800c294e6559a4a587
David-mwas <dmwas704@gmaIl.com>
2025-01-12 18:35:10 +0300

    FIX : static path path

--- api/index.go
+++ api/index.go
@@ -48,7 +48,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func handler() http.Handler {
 	app := fiber.New()
 
-	// Serve static files from the Vite build (dist folder) in production
+	// Serve static files from the Vite build (dist folder)
 	if os.Getenv("ENV") == "production" {
 		// Serve all the static files in the dist folder
 		app.Static("/", "./client/dist")
